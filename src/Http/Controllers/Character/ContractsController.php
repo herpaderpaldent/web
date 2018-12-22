516a5193d16033e1c25203476375218fa61f18b3
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-22 09:23:20 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Character/ContractsController.php
+++ src/Http/Controllers/Character/ContractsController.php
@@ -161,7 +161,7 @@ class ContractsController extends Controller
             })
             ->addColumn('contents', function ($row) {
 
-                $url = route('character.view.contracts.items', ['character_id' => $row->character_id, 'contract_id' => $row->contract_id ]);
+                $url = route('character.view.contracts.items', ['character_id' => $row->character_id, 'contract_id' => $row->contract_id]);
 
                 return view('web::partials.contractcontentsbutton', compact('row', 'url'));
             })
